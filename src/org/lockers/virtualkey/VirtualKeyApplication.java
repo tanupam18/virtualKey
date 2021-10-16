92898d93330157c5aab56ab578facc930e391798
tanupam18 <tanupam18@gmail.com>
2021-10-16 21:34:55 +0530

    Adding the welcome Screen in the project

---
+++ src/org/lockers/virtualkey/VirtualKeyApplication.java
@@ -0,0 +1,12 @@
+package org.lockers.virtualkey;
+import org.lockers.virtualkey.KeyInterface.StartScreen;
+
+public class VirtualKeyApplication {
+
+	public static void main(String[] args) {
+		StartScreen startscreen = new StartScreen();
+		startscreen.entryPoint();
+
+	}
+
+}
